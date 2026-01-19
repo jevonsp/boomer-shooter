a12f3e1a6ca81a3d4ea04d6312716bf43f3f2f65
jevonsp <pineapplesituation@gmail.com>
2026-01-19 15:48:31 -0800

    Corrected Blood Spawning

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -1,8 +1,9 @@
 extends CharacterBody3D
+const BLOOD = preload("res://enemies/blood.tscn")
 const SPEED = 1.0
-var hitpoints: int = 5
+var hitpoints: int = 1000
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
-@onready var blood: GPUParticles3D = $zombie/Blood
+
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -32,6 +33,16 @@ func move_to_player():
 	look_at(flat_target, Vector3.UP)
 
 func blood_splatter():
+	var blood: GPUParticles3D = BLOOD.instantiate()
+	
+	var root = get_tree().root
+	
+	blood.position = position + Vector3(0, 1, 0)
+	
+	root.add_child(blood)
+	
+	print("blood pos=%s" % blood.position)
 	blood.emitting = true
 	
+	blood.finished.connect(func(): blood.queue_free())
 	
